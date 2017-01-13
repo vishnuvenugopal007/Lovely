506c778150c3bc051bfd421475480183dcca0f2c
Vishnu <vishnu_venugopal@live.com>
2017-01-13 10:52:14 -0800

    Created Token requirement (taken from environment prompt)

--- slack_bot.js
+++ slack_bot.js
@@ -1,8 +1,16 @@
 /*eslint no-console: 'off'*/
 /* eslint no-unused-vars: 'off'*/
+if (!process.env.token) {
+  console.log('Error: Specify token in environment');
+  process.exit();
+}
 
 var Botkit = require('botkit');
 
 var controller = Botkit.slackbot({
   debug: true,
 });
+
+var bot = controller.spawn({
+  token: process.env.token
+}).startRTM();
