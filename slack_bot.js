1bd9ca87139d4db69cc5acb4834620fb0d71a28d
Vishnu <vishnu_venugopal@live.com>
2017-01-13 13:46:01 -0800

    Lovely can learn a user's name.

--- slack_bot.js
+++ slack_bot.js
@@ -21,3 +21,17 @@ controller.hears(['hello', 'hi'], 'direct_message,direct_mention,mention', funct
     }
   )
 })
+controller.hears(['call me (.*)', 'my name is (.*)'], 'direct_message,direct_mention,mention', function(bot, message){
+  const name = message.match[1];
+  controller.storage.users.get(message.user, function(err, user) {
+    if(!user) {
+      user = {
+        id: message.user,
+      }
+    }
+    user.name = name;
+    controller.storage.users.save(user, function(err, id) {
+      bot.reply(message, 'Pleased to meet you ' + user.name + '. How\'\s your day been?')
+    })
+  })
+})
