959ba1ce1e31ea4792f38c4f2571aed8ccd18cf4
Vishnu <vishnu_venugopal@live.com>
2017-01-13 13:02:18 -0800

    Lovely responds to 'hello' and 'hi' with 'Hello I'm Lovely! How are you?'

--- slack_bot.js
+++ slack_bot.js
@@ -9,8 +9,15 @@ var Botkit = require('botkit');
 
 var controller = Botkit.slackbot({
   debug: true,
-});
+})
 
 var bot = controller.spawn({
   token: process.env.token
 }).startRTM();
+
+controller.hears(['hello', 'hi'], 'direct_message,direct_mention,mention', function(bot,message){
+  controller.storage.users.get(message.user, function(err, user) {
+    bot.reply(message, 'Hello I\'\m Lovely! How are you?');
+    }
+  )
+})
