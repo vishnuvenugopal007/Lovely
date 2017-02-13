34717b0e4f8eb47d88e1d457de5cf47620f4778d
Vishnu <vishnu_venugopal@live.com>
2017-02-13 14:38:07 -0800

    added arrays and timers

--- slack_bot.js
+++ slack_bot.js
@@ -1,6 +1,5 @@
 /* eslint no-console: 'off' */
 /* eslint no-unused-vars: 'off'*/
-
 const Botkit = require('botkit');
 var controller = Botkit.slackbot({
   debug: false,
@@ -26,24 +25,6 @@ controller.hears(['hello', 'hey', 'what\'s up'], 'direct_message,direct_mention,
     else{
       bot.reply(message, 'Hello I\'m Lovely! How are you?');
     }
-
-    bot.reply(message,{
-      attachment: {
-        'text': 'Do you mind if I send you some love in the mornings?',
-        'payload':{
-            'buttons': [
-              {
-                'type': 'submit',
-                'title': 'Yes'
-              },
-              {
-                'type' : 'submit',
-                'title': 'No'
-              }
-            ]
-          }
-        }
-    })
 })
 controller.hears(['call me (.*)', 'my name is (.*)'], 'direct_message,direct_mention,mention', function(bot, message){
   //Got response null {"ok":true,"channel":"D3PRE3V6E","ts":"1484349146.000035","message":{"type":"message","user":"U3P4C8G2C","text":"Pleased to meet you john. How's your day been?","bot_id":"B3P4C8FL0","ts":"1484349146.000035"}}
@@ -72,7 +53,7 @@ controller.hears(['good', 'great', 'loving life'], 'direct_message,direct_mentio
     channel: message.channel,
     name: 'ok_hand::skin-tone-5',
   })
-  bot.reply(message, 'As Tony the Tiger would say, that\'s GRRRRREAT (like frosted flakes).')
+  bot.reply(message, 'As Tony the Tiger would say, that\'s GRRRRREAT (like frosted flakes). If you ever need a reminder or some advice, just let me know!')
 })
 
 controller.hears(['bad', 'not well', 'sad'], 'direct_message,direct_mention,mention', function(bot, message){
@@ -82,7 +63,7 @@ controller.hears(['bad', 'not well', 'sad'], 'direct_message,direct_mention,ment
     channel: message.channel,
     name: 'hushed',
   })
-  bot.reply(message, 'Oh no, what\'s wrong? I\'m here if you want to talk about anything.')
+  bot.reply(message, 'Oh no, what\'s wrong? I\'m here if you want to talk about anything.You can ask me for reminders or advice if you\'re feeling down too.')
 })
 })
 
@@ -109,7 +90,7 @@ controller.hears(['Can you help me?', 'help', 'help me!'], 'direct_message,direc
     name: 'thumbsup::skin-tone-5',
   })
 
-  bot.reply(message, 'Sure! what do you need?')
+  bot.reply(message, 'Sure! What do you need?')
 })
 
 //Who are you?
@@ -121,7 +102,70 @@ controller.hears(['What\'s your name', 'Who are you','What\'s your purpose', 'Wh
     channel: message.channel,
     name: 'hugging_face',
   })
-  bot.reply(message, 'My name is Lovely, and my job is to help you remember to love yourself <3')
+  bot.reply(message, 'My name is Lovely, and my job is to help you remember to love yourself :heart:')
+  setTimeout(function() {
+    bot.reply(message, 'I can also send you some :fire: music.')
+  }, 3000)
+  setTimeout(function() {
+    bot.reply(message, 'Lastly, if you need a lovely reminder or some advice, just let me know by saying `I need a reminder` or `I need some advice`')
+  }, 5000)
+})
+
+//Reminder & Advice functionality
+
+const reminders = [
+  'You are loved and you are love. :heart:',
+  '< https://www.youtube.com/watch?v=0j8frgmdvgc | This> exists, so things can\'t be that bad...',
+  'How many different things had to happen for you to be here right now? Seems like a lot more than just happy coincidences :)',
+  'Watch <https://www.youtube.com/watch?v=y6Sxv-sUYtM | this video> and let me know if you need anything else :)',
+  'Check <https://www.youtube.com/watch?v=09R8_2nJtjg | this> out ;)',
+  'You are dope AF. No one else knows you like you do, so if they can\'t see the wave, it\'s their own loss.',
+  'Get up and < https://www.youtube.com/watch?v=C_9HNEyLa4U | groove> for a minute. It might help your mood ;)',
+  'You are loved and you are love. No one can take that away from you, no matter what they say or do. Find your way through by looking inside you, I promise it gets better if you focus on working through all the pain that these lames put inside you.'
+]
+
+//const randomReminder = reminders[Math.floor(Math.random() * reminders.length)]
+
+const advice = [
+  'Will this matter six months from now?',
+  'There\'s a lot that can break your focus, but ask yourself: what would Beyonce do?',
+  'Stituations are rarely as extreme and absolute as they seem. Perhaps this is more grey than black and white?',
+  'Stay low and build. Let your success be your revenge.',
+  'Is there another way to look at the situation?',
+  'Are you being fair and compassionate with yourself, the way a close friend would be?',
+  'How would a third party see your current situation? Would they be critical or kind? Caring or harsh?',
+]
+
+//const randomAdvice = advice[Math.floor(Math.random() * advice.length)]
+
+controller.hears([ 'I need a reminder', 'Remind me of my worth'], 'direct_message,direct_mention,mention', (bot,message) => {
+  console.log('HERE: ' +JSON.stringify(message))
+  bot.api.reactions.add({
+    timestamp: message.ts,
+    channel: message.channel,
+    name: 'heartpulse'
+  })
+
+  bot.reply(message, randomReminder)
+})
+
+controller.hears([ 'I need some advice' ], 'direct_message,direct_mention,mention', (bot,message) => {
+  bot.api.reactions.add({
+    timestamp: message.ts,
+    channel: message.channel,
+    name: 'thinking_face'
+  })
+    setTimeout(function () {
+      bot.api.reactions.add({
+        timestamp: message.ts,
+        channel: message.channel,
+        name: 'bulb'
+    }), 3000})
+
+
+  setTimeout(function() {
+    bot.reply(message, randomAdvice)
+  }, 5000)
 })
 
 //Music suggestion functionality
@@ -150,13 +194,16 @@ controller.hears(['hip-hop', 'hip hop', 'rap'], 'direct_message,direct_mention,m
     timestamp: message.ts,
     channel: message.channel,
     name: 'thinking_face'
-  })
-  bot.api.reactions.add({
+    })
+  setTimeout( function() {
+    bot.api.reactions.add({
     timestamp: message.ts,
     channel: message.channel,
     name: 'bulb'
-  })
-  bot.reply(message, 'For sure, check out this playlist: <https://www.youtube.com/playlist?list=PLpHBQZMQf9d5g_gvLsQtvENgahwd5vdwX | Lovely Hip hop>')
+  })}, 1000);
+  setTimeout(function() {
+    bot.reply(message, 'I got just the thing, check out this playlist: <https://www.youtube.com/playlist?list=PLpHBQZMQf9d5g_gvLsQtvENgahwd5vdwX | Lovely Hip hop>')
+  }, 3000)
 })
 
 //Pop & Top 40 & Radio
@@ -168,13 +215,18 @@ controller.hears(['pop', 'top 40', 'radio'], 'direct_message,direct_mention,ment
     channel: message.channel,
     name: 'thinking_face'
     })
-  bot.api.reactions.add({
+  setTimeout( function() {
+    bot.api.reactions.add({
     timestamp: message.ts,
     channel: message.channel,
     name: 'bulb'
-  })
-  bot.reply(message, 'Ok, let me know what you think of this: <https://www.youtube.com/playlist?list=PLpHBQZMQf9d7CB50Q450W6G-vm4fto88k | Lovely Pop>')
+  })}, 1000);
+
+  setTimeout(function() {
+    bot.reply(message, 'Ok, let me know what you think of this: <https://www.youtube.com/playlist?list=PLpHBQZMQf9d7CB50Q450W6G-vm4fto88k | Lovely Pop>')
+  }, 3000)
 })
+
 
 //Poetry & Slam Poetry
 
@@ -184,13 +236,17 @@ controller.hears(['spoken word', 'poetry', 'slam poetry', 'poems'], 'direct_mess
     timestamp: message.ts,
     channel: message.channel,
     name: 'thinking_face'
-  })
-  bot.api.reactions.add({
+    })
+  setTimeout( function() {
+    bot.api.reactions.add({
     timestamp: message.ts,
     channel: message.channel,
     name: 'bulb'
-  })
-  bot.reply(message, 'Yaaaaaas. Maybe this will get you snapping : <https://www.youtube.com/playlist?list=PLpHBQZMQf9d7t123LqvNGhXcgr7yGlCJd | Lovely Spoken Word>')
+  })}, 1000);
+
+  setTimeout(function() {
+    bot.reply(message, 'Yaaaaaas. Maybe this will get you snapping : <https://www.youtube.com/playlist?list=PLpHBQZMQf9d7t123LqvNGhXcgr7yGlCJd | Lovely Spoken Word>')
+  }, 3000)
 })
 
 //R&B/Soul
@@ -201,11 +257,35 @@ controller.hears(['R & B', 'Rhythm and Blues', 'Soul', 'Neo-Soul'], 'direct_mess
     timestamp: message.ts,
     channel: message.channel,
     name: 'thinking_face'
-  })
-  bot.api.reactions.add({
+    })
+  setTimeout( function() {
+    bot.api.reactions.add({
     timestamp: message.ts,
     channel: message.channel,
     name: 'bulb'
-  })
-  bot.reply(message, 'Oooh I see you. Maybe this will help you find your own smooth operator: <https://www.youtube.com/playlist?list=PLpHBQZMQf9d5CH-upXX6J9lrzAcx_WSKX | Lovely R&B/Soul>')
+  })}, 1000);
+
+  setTimeout(function() {
+    bot.reply(message, 'Oooh I see you. Maybe this will help you find your own smooth operator: <https://www.youtube.com/playlist?list=PLpHBQZMQf9d5CH-upXX6J9lrzAcx_WSKX | Lovely R&B/Soul>')
+  }, 3000)
+})
+//electronic
+
+controller.hears(['EDM', 'Electronic', 'Dance'], 'direct_message,direct_mention,mention', function(bot, message) {
+  console.log('HERE: ' +JSON.stringify(message))
+  bot.api.reactions.add({
+    timestamp: message.ts,
+    channel: message.channel,
+    name: 'thinking_face'
+    })
+  setTimeout( function() {
+    bot.api.reactions.add({
+    timestamp: message.ts,
+    channel: message.channel,
+    name: 'bulb'
+  })}, 1000);
+
+  setTimeout(function() {
+  bot.reply(message, 'Get lost in this playlist: <https://www.youtube.com/playlist?list=PLpHBQZMQf9d7p9w5wBjA6Fpn87qpEDB4k | Lovely Electronica>')
+  }, 3000)
 })
